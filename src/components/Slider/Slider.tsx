199835a6b3d7251ca89cdca5089a936ebce8e473
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2023-12-04 14:29:02 +0200

    fix: slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,44 +1,173 @@
-import { useState } from "react";
 import "./Slider.styled.css";
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
+import { ArrowRightTop } from "../../ui/svgElements/ArrowRightTop";
+import { ArrowSliderLeft } from "../../ui/svgElements/ArrowSliderLeft";
+import { ArrowSliderRight } from "../../ui/svgElements/ArrowSliderRight";
 
-const slidesData = [
-  "./assets/image/beautiful-view-wind-turbines-grass-covered-field.jpg",
-  "./assets/image/solar-battery.jpg",
-  "./assets/image/rivne-biotech.jpg",
-  "./assets/image/kherson-helthy-farm.jpg",
-  "./assets/image/zaporizhia-biotech.jpg",
-];
 export const Slider = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [ref, slider] = useKeenSlider<HTMLDivElement>({
+    loop: true,
+  });
+  let currentSlide: number | null = null;
+  if (slider.current) {
+    // console.log(slider.current.track.details.rel);
+    currentSlide =
+      slider.current.slides.length - (slider.current.slides.length - 1);
+  }
+  let numbersOfSlides = 0;
+  if (slider.current) {
+    numbersOfSlides = slider.current.track.details.slidesLength;
+  }
 
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % slidesData.length);
+  const handlePrev = () => {
+    currentSlide = slider.current.track.details.rel;
+    if (slider) {
+      slider.current.prev();
+    }
   };
 
-  const prevSlide = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? slidesData.length - 1 : prevIndex - 1
-    );
+  const handleNext = () => {
+    currentSlide = slider.current.track.details.rel;
+    if (slider) {
+      slider.current.next();
+    }
   };
-
   return (
-    <div className="slider-container">
-      <button onClick={prevSlide} className="arrow left">
-        &#60;
-      </button>
-      <div className="slider">
-        {slidesData.map((slide, index) => (
-          <div
-            key={index}
-            className={`slide ${index === currentIndex ? "active" : ""}`}
-          >
-            <img src={slide} alt={`slide-${index}`} />
-          </div>
-        ))}
+    <>
+      <div className="control-box-wrap">
+        <p>
+          0{currentSlide + 1}
+          <span className="slides-numbers">/0{numbersOfSlides}</span>
+        </p>
+        <div className="control-btn-wrap">
+          <button className="control-btn" onClick={handlePrev}>
+            <ArrowSliderLeft />
+          </button>
+          <button className="control-btn" onClick={handleNext}>
+            <ArrowSliderRight />
+          </button>
+        </div>
       </div>
-      <button onClick={nextSlide} className="arrow right">
-        &#62;
-      </button>
-    </div>
+      <div ref={ref} className="keen-slider">
+        <div className="keen-slider__slide number-slide1">
+          <div className="slide-thumb-image">
+            <img
+              src="./assets/image/beautiful-view-wind-turbines-grass-covered-field.jpg"
+              alt="wind turbines"
+              className="slide-image"
+            />
+          </div>
+          <div className="case">
+            <div className="case-location-wrap">
+              <p className="case-location">
+                Lviv Region, Radekhiv town Private Enterprise “ZAKHIDNYI BUH”
+              </p>
+              <a href="#">
+                <ArrowRightTop />
+              </a>
+            </div>
+            <div className="case-description-wrap">
+              <p className="case-location">
+                Wind Power for auto field irrigation
+              </p>
+              <p>July 2023</p>
+            </div>
+          </div>
+        </div>
+        <div className="keen-slider__slide number-slide2">
+          <div className="slide-thumb-image">
+            <img
+              src="./assets/image/solar-battery.jpg"
+              alt="Solar Panels"
+              className="slide-image"
+            />
+          </div>
+          <div className="case">
+            <div className="case-location-wrap">
+              <p className="case-location">
+                Zhytomyr city Private Enterprise “Bosch”
+              </p>
+              <a href="#">
+                <ArrowRightTop />
+              </a>
+            </div>
+            <div className="case-description-wrap">
+              <p className="case-location">Solar Panels for industrial use</p>
+              <p>November 2023</p>
+            </div>
+          </div>
+        </div>
+        <div className="keen-slider__slide number-slide3">
+          <div className="slide-thumb-image">
+            <img
+              src="./assets/image/rivne-biotech.jpg"
+              alt="Thermal modules"
+              className="slide-image"
+            />
+          </div>
+          <div className="case">
+            <div className="case-location-wrap">
+              <p className="case-location">
+                Rivne city Private Enterprise “Biotech”
+              </p>
+              <a href="#">
+                <ArrowRightTop />
+              </a>
+            </div>
+            <div className="case-description-wrap">
+              <p className="case-location">Thermal modules</p>
+              <p>October 2023</p>
+            </div>
+          </div>
+        </div>
+        <div className="keen-slider__slide number-slide4">
+          <div className="slide-thumb-image">
+            <img
+              src="./assets/image/kherson-helthy-farm.jpg"
+              alt="wind turbines"
+              className="slide-image"
+            />
+          </div>
+          <div className="case">
+            <div className="case-location-wrap">
+              <p className="case-location">
+                Kherson city Private Enterprise “HealthyFarm”
+              </p>
+              <a href="#">
+                <ArrowRightTop />
+              </a>
+            </div>
+            <div className="case-description-wrap">
+              <p className="case-location">Wind power</p>
+              <p>September 2021</p>
+            </div>
+          </div>
+        </div>
+        <div className="keen-slider__slide number-slide5">
+          <div className="slide-thumb-image">
+            <img
+              src="./assets/image/zaporizhia-biotech.jpg"
+              alt="Mini nuclear stations"
+              className="slide-image"
+            />
+          </div>
+          <div className="case">
+            <div className="case-location-wrap">
+              <p className="case-location">
+                Zaporizhia city Private Enterprise “Biotech”
+              </p>
+              <a href="#">
+                <ArrowRightTop />
+              </a>
+            </div>
+            <div className="case-description-wrap">
+              <p className="case-location">Mini nuclear stations</p>
+              <p>May 2021</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
---
+++ src/ui/svgElements/ArrowRightTop.tsx
@@ -0,0 +1,32 @@
+import "../../components/Slider/Slider.styled.css";
+
+export function ArrowRightTop() {
+  return (
+    <svg
+      className="arrow-right-top"
+      width="60"
+      height="60"
+      viewBox="0 0 60 60"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="60" height="60" rx="30" fill="#97D28B" />
+      <path
+        d="M26.9976 22.9867L37.0125 22.9867L37.0125 33.0017"
+        stroke="#173D33"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M22.9888 37.0109L36.8728 23.1269"
+        stroke="#173D33"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
