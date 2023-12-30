298df065d4a2e0ba99ffd53e6196c551cc78c7fe
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2023-12-30 21:52:43 +0200

    add: count in electricity

--- src/components/Electricity/Electricity.tsx
+++ src/components/Electricity/Electricity.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { SectionTitle } from "../../ui/SectionTitle/SectionTitle";
 import {
   Count,
@@ -6,15 +7,30 @@ import {
   ElectricitySection,
   ElectricityTitleWrap,
 } from "./Electricity.styled";
+import { formatNumberWithDots } from "../../helpers/formatNumberWithDots";
 
 export function Electricity() {
+  const [count, setCount] = useState(1134147814);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((pervState) => pervState + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const formatedCountNumber: string = formatNumberWithDots(count);
+
   return (
     <ElectricitySection>
       <ElectricityTitleWrap>
         <SectionTitle>Electricity we produced for all time</SectionTitle>
       </ElectricityTitleWrap>
       <CountWrap>
-        <Count>1.134.147.814</Count>
+        <Count>{formatedCountNumber}</Count>
         <CountUnit>kWh</CountUnit>
       </CountWrap>
     </ElectricitySection>
