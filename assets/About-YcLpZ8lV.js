ed29b55adcff490f5a39e1f9107fe6dba3beb354
mantr88 <mantr88@users.noreply.github.com>
2024-01-04 15:02:59 +0000

    deploy: 47c234ed7f9c8016ae0a5024adff305f19271dea

---
+++ assets/About-YcLpZ8lV.js
@@ -0,0 +1,179 @@
+import{j as e,s as t,S as a}from"./index-TSVlhL0L.js";function d(){return e.jsxs("svg",{className:"aboutItemIcon",width:"16",height:"16",viewBox:"0 0 16 16",fill:"none",xmlns:"http://www.w3.org/2000/svg",children:[e.jsx("path",{d:"M6.40008 13.3333H9.60008C12.2667 13.3333 13.3334 12.2666 13.3334 9.59996V6.39996C13.3334 3.73329 12.2667 2.66663 9.60008 2.66663H6.40008C3.73341 2.66663 2.66675 3.73329 2.66675 6.39996V9.59996C2.66675 12.2666 3.73341 13.3333 6.40008 13.3333Z",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M7.00008 11.3333H9.00008C10.6667 11.3333 11.3334 10.6666 11.3334 8.99996V6.99996C11.3334 5.33329 10.6667 4.66663 9.00008 4.66663H7.00008C5.33341 4.66663 4.66675 5.33329 4.66675 6.99996V8.99996C4.66675 10.6666 5.33341 11.3333 7.00008 11.3333Z",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M5.34009 2.66671V1.33337",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M8 2.66671V1.33337",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M10.6667 2.66671V1.33337",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M13.3333 5.33337H14.6666",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M13.3333 8H14.6666",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M13.3333 10.6666H14.6666",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M10.6667 13.3334V14.6667",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M8.00659 13.3334V14.6667",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M5.34009 13.3334V14.6667",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M1.33325 5.33337H2.66659",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M1.33325 8H2.66659",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M1.33325 10.6666H2.66659",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M7.9999 6.46667L7.37323 7.56001C7.23323 7.80001 7.34656 8.00001 7.62656 8.00001H8.37323C8.65323 8.00001 8.76656 8.20001 8.62656 8.44001L7.9999 9.53334",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"})]})}function p(){return e.jsxs("svg",{className:"aboutItemIcon",width:"16",height:"16",viewBox:"0 0 16 16",fill:"none",xmlns:"http://www.w3.org/2000/svg",children:[e.jsx("path",{d:"M14.6666 8.00004C14.6666 4.32004 11.6799 1.33337 7.99992 1.33337C4.31992 1.33337 1.33325 4.32004 1.33325 8.00004C1.33325 11.68 4.31992 14.6667 7.99992 14.6667",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M5.33324 2H5.9999C4.6999 5.89333 4.6999 10.1067 5.9999 14H5.33324",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M10 2C10.6467 3.94667 10.9733 5.97333 10.9733 8",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M2 10.6667V10C3.94667 10.6467 5.97333 10.9733 8 10.9733",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M2 5.99978C5.89333 4.69978 10.1067 4.69978 14 5.99978",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M12.8068 10.4933L10.4468 12.8534C10.3534 12.9467 10.2668 13.12 10.2468 13.2467L10.1201 14.1467C10.0734 14.4733 10.3001 14.7 10.6268 14.6533L11.5268 14.5267C11.6534 14.5067 11.8334 14.42 11.9201 14.3267L14.2801 11.9667C14.6868 11.56 14.8801 11.0867 14.2801 10.4867C13.6868 9.89334 13.2134 10.0867 12.8068 10.4933Z",stroke:"#173D33",strokeMiterlimit:"10",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M12.4668 10.8334C12.6668 11.5534 13.2268 12.1134 13.9468 12.3134",stroke:"#173D33",strokeMiterlimit:"10",strokeLinecap:"round",strokeLinejoin:"round"})]})}function l(){return e.jsxs("svg",{className:"aboutItemIcon",width:"16",height:"16",viewBox:"0 0 16 16",fill:"none",xmlns:"http://www.w3.org/2000/svg",children:[e.jsx("path",{d:"M11.3333 2H13.9999V4.66667",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M10 6L14 2",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M4.66667 14H2V11.3334",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M6 10L2 14",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M9.3466 1.46672C8.91327 1.38005 8.45992 1.33337 7.99992 1.33337C4.31992 1.33337 1.33325 4.32004 1.33325 8.00004C1.33325 8.46004 1.37993 8.90671 1.46659 9.33337",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M6.65332 14.5333C7.08665 14.62 7.54 14.6666 8 14.6666C11.68 14.6666 14.6667 11.68 14.6667 7.99996C14.6667 7.54663 14.62 7.09996 14.5333 6.66663",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"})]})}function c(){return e.jsxs("svg",{className:"aboutItemIcon",width:"16",height:"16",viewBox:"0 0 16 16",fill:"none",xmlns:"http://www.w3.org/2000/svg",children:[e.jsx("path",{d:"M5.77992 9.33337H2.66659C1.93325 9.33337 1.33325 9.93337 1.33325 10.6667V14.6667H5.77992V9.33337Z",stroke:"#173D33",strokeMiterlimit:"10",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M8.88677 6.66663H7.10677C6.37344 6.66663 5.77344 7.26663 5.77344 7.99996V14.6666H10.2201V7.99996C10.2201 7.26663 9.62677 6.66663 8.88677 6.66663Z",stroke:"#173D33",strokeMiterlimit:"10",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M13.3333 11.3334H10.22V14.6667H14.6666V12.6667C14.6666 11.9334 14.0666 11.3334 13.3333 11.3334Z",stroke:"#173D33",strokeMiterlimit:"10",strokeLinecap:"round",strokeLinejoin:"round"}),e.jsx("path",{d:"M8.34658 1.38L8.69991 2.08667C8.74658 2.18667 8.87324 2.28 8.97991 2.29333L9.61991 2.4C10.0266 2.46667 10.1266 2.76666 9.83324 3.05332L9.33324 3.55332C9.24658 3.63999 9.19991 3.8 9.22658 3.91333L9.36658 4.52667C9.47991 5.01334 9.21991 5.20001 8.79324 4.94668L8.19324 4.59334C8.08658 4.52668 7.90658 4.52668 7.79991 4.59334L7.19991 4.94668C6.77324 5.20001 6.51324 5.01334 6.62657 4.52667L6.76658 3.91333C6.79324 3.8 6.74658 3.63332 6.65991 3.55332L6.16658 3.06C5.87324 2.76666 5.96658 2.47332 6.37991 2.40666L7.01991 2.30001C7.12657 2.28001 7.25324 2.18668 7.29991 2.09334L7.65324 1.38666C7.84658 0.99999 8.15324 1 8.34658 1.38Z",stroke:"#173D33",strokeLinecap:"round",strokeLinejoin:"round"})]})}const h=t.ul`
+  display: grid;
+  gap: 24px;
+  grid-template-columns: repeat(2, 1fr);
+  margin-top: 36px;
+
+  @media screen and (min-width: ${n=>n.theme.breakpoints.m}) {
+    grid-template-columns: repeat(4, 1fr);
+    margin-top: 100px;
+  }
+
+  @media screen and (min-width: ${n=>n.theme.breakpoints.l}) {
+    gap: 48px;
+  }
+`,r=t.li`
+  height: 197px;
+  padding: 13px 12px 12px 12px;
+
+  background: ${n=>n.theme.colors.second_bg};
+
+  @media screen and (min-width: ${n=>n.theme.breakpoints.m}) {
+    width: 159px;
+    padding-top: 12px;
+  }
+
+  @media screen and (min-width: ${n=>n.theme.breakpoints.l}) {
+    width: 274px;
+    height: 339px;
+    padding: 48px 24px 48px 24px;
+  }
+`,m=t(r)``,x=t(r)``,u=t.div`
+  display: none;
+
+  @media screen and (min-width: ${n=>n.theme.breakpoints.m}) {
+    display: block;
+    grid-column: span 2;
+    width: 342px;
+  }
+
+  @media screen and (min-width: ${n=>n.theme.breakpoints.l}) {
+    width: 596px;
+  }
+`,k=t.div`
+  display: none;
+
+  @media screen and (min-width: ${n=>n.theme.breakpoints.m}) {
+    display: block;
+    grid-column: span 2;
+    width: 342px;
+  }
+
+  @media screen and (min-width: ${n=>n.theme.breakpoints.l}) {
+    width: 596px;
+  }
+`,j=t(r)``,g=t(r)``,s=t.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  gap: 8px;
+  padding-bottom: 33px;
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 57px;
+    width: 100%;
+    height: 1px;
+    background: ${n=>n.theme.colors.accent};
+  }
+
+  @media screen and (min-width: ${n=>n.theme.breakpoints.l}) {
+    padding-bottom: 0;
+    &::after {
+      top: 119px;
+      width: 226px;
+    }
+  }
+`,i=t.h4`
+  font-family: 'Oswald';
+  font-weight: 400;
+  line-height: 1.5;
+  text-transform: uppercase;
+
+  @media screen and (min-width: ${n=>n.theme.breakpoints.l}) {
+    font-size: 32px;
+  }
+`,o=t.p`
+  margin-top: 12px;
+  text-align: justify;
+  font-size: 14px;
+  letter-spacing: -0.56px;
+
+  @media screen and (min-width: ${n=>n.theme.breakpoints.l}) {
+    margin-top: 119px;
+    font-size: 16px;
+  }
+`;function w(){return e.jsxs(h,{children:[e.jsxs(m,{children:[e.jsxs(s,{children:[e.jsx(l,{}),e.jsx(i,{children:"Openness"})]}),e.jsx(o,{children:"to the world, people, new ideas and projects"})]}),e.jsxs(x,{children:[e.jsxs(s,{children:[e.jsx(p,{}),e.jsx(i,{children:"Responsibility"})]}),e.jsx(o,{children:"twe are aware that the results of our work have an impact on our lives and the lives of future generations"})]}),e.jsx(u,{children:e.jsx("img",{srcSet:"./assets/image/wind-farms-fields-342.jpg 320w, ./assets/image/wind-farms-fields-684.jpg 640w, ./assets/image/wind-farms-fields-1026.jpg 1024w, ./assets/image/wind-farms-fields-1788.jpg 1440w",src:"./assets/image/wind-farms-fields-342.jpg",alt:"wind farms workers"})}),e.jsx(k,{children:e.jsx("img",{srcSet:"./assets/image/man-worker-firld-by-solar-panels-342.jpg 320w, ./assets/image/man-worker-firld-by-solar-panels-684.jpg 640w, ./assets/image/man-worker-firld-by-solar-panels-1026.jpg 1024w, ./assets/image/man-worker-firld-by-solar-panels-1788.jpg 1440w",src:"./assets/image/man-worker-firld-by-solar-panels-342.jpg",alt:"solar farms workers"})}),e.jsxs(j,{children:[e.jsxs(s,{children:[e.jsx(d,{}),e.jsx(i,{children:"Innovation"})]}),e.jsx(o,{children:"we use the latest technology to implement non-standard solutions"})]}),e.jsxs(g,{children:[e.jsxs(s,{children:[e.jsx(c,{}),e.jsx(i,{children:"Quality"})]}),e.jsx(o,{children:"we do not strive to be the first among others, but we want to be the best in our business"})]})]})}const L=t.section`
+  margin-top: 24px;
+
+  @media screen and (min-width: ${n=>n.theme.breakpoints.m}) {
+    margin-top: 40px;
+  }
+
+  @media screen and (min-width: ${n=>n.theme.breakpoints.l}) {
+    margin-top: 36px;
+  }
+`,f=t.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  letter-spacing: -0.64px;
+
+  @media screen and (min-width: ${n=>n.theme.breakpoints.m}) {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+`,b=t.p`
+  display: none;
+
+  @media screen and (min-width: ${n=>n.theme.breakpoints.m}) {
+    display: block;
+  }
+`,C=t.div`
+  margin-top: 36px;
+
+  @media screen and (min-width: ${n=>n.theme.breakpoints.m}) {
+    margin-top: 40px;
+  }
+`,M=t.div`
+  margin-top: 36px;
+
+  @media screen and (min-width: ${n=>n.theme.breakpoints.m}) {
+    display: flex;
+    justify-content: space-between;
+    align-items: start;
+    /* gap: 94px; */
+    margin-top: 100px;
+  }
+
+  @media screen and (min-width: ${n=>n.theme.breakpoints.l}) {
+    margin-top: 120px;
+  }
+`,y=t.div`
+  @media screen and (min-width: ${n=>n.theme.breakpoints.l}) {
+    width: 390px;
+  }
+`,v=t.p`
+  text-align: justify;
+  letter-spacing: -0.64px;
+  margin-top: 36px;
+
+  @media screen and (min-width: ${n=>n.theme.breakpoints.m}) {
+    position: relative;
+    width: 396px;
+    margin-top: 0;
+
+    &::before {
+      content: " ";
+      position: absolute;
+      top: 4px;
+      left: -11px;
+      width: 1px;
+      height: 110px;
+      background: ${n=>n.theme.colors.accent};
+    }
+  }
+
+  @media screen and (min-width: ${n=>n.theme.breakpoints.l}) {
+    width: 459px;
+
+    &::before {
+      height: 87px;
+      left: -161px;
+    }
+  }
+`;function V(){return e.jsxs(L,{id:"about-section",children:[e.jsxs(f,{children:[e.jsx("a",{href:"https://maps.app.goo.gl/2WzseMV8Ymfioi3p7","aria-label":"Link for see the location of the office company",children:"79005, Ukraine, Lvivstreet. Shota Rustaveli, 7"}),e.jsxs("a",{href:"mailto:office@ecosolution.com","aria-label":"Link to go to the service to send the email ",children:["office@ecosolution.com"," "]}),e.jsx(b,{children:"ecosolution © 2023"})]}),e.jsx(C,{children:e.jsx("img",{srcSet:"./assets/image/wind-turbine-clean-energy-320.jpg 320w, ./assets/image/wind-turbine-clean-energy-640.jpg 640w, ./assets/image/wind-turbine-clean-energy-960.jpg 960w,./assets/image/wind-turbine-clean-energy-1240.jpg 1280w,./assets/image/wind-turbine-clean-energy-2480.jpg 2560w",src:"./assets/image/wind-turbine-clean-energy-320.jpg",sizes:"100vw",alt:"wind turbine"})}),e.jsxs(M,{children:[e.jsx(y,{children:e.jsx(a,{children:"Main values of our company"})}),e.jsx(v,{children:"EcoSolution envisions a world where sustainable energy solutions power a brighter and cleaner future for all. We aspire to be at the forefront of the global shift towards renewable energy, leading the way in innovative technologies that harness the power of nature to meet the world's energy needs."})]}),e.jsx(w,{})]})}export{V as default};
